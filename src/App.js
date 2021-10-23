d9eb6e56e192a8570485e6ccc6dc1f52e2c13157
MehdiShu <mehdidevelop3@gmail.com>
2021-10-23 12:43:04 +0330

    Set props option in App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Card from './Card'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Card title="Article 1" body="this is article 1" />
+      <Card title="Article 2" body="this is article 2" />
+      <Card title="Article 3" body="this is article 3" />
     </div>
   );
 }
---
+++ src/Card.js
@@ -0,0 +1,39 @@
+import React , {Component} from "react";
+
+// function Card({title , body}) {
+//     return (
+//         <div className="card">
+//             <header className="card-header">
+//             <h2>{title}</h2>
+//             </header>
+//             <section className="card-body">
+//                 <p>{body}</p>
+//             </section>
+//             <footer>
+//                 <p>Card Footer</p>
+//             </footer>
+//         </div>
+//     )
+// }
+
+class Card extends Component {
+    render(){
+        let {title , body} = this.props;
+        return (
+            <div className="card">
+                <header className="card-header">
+                <h2>{title}</h2>
+                </header>
+                <section className="card-body">
+                <p>{body}</p>
+                </section>
+                <footer>
+                <p>Card Footer</p>
+                </footer>
+            </div>
+                )
+    }
+}
+
+
+export default Card;
